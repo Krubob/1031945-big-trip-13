e73e2a49a46245c238f0325e32269cbc479c9e6f
Vladislav <res.19@yandex.ru>
2020-12-21 21:58:20 +0300

    Добавляет рефакторинг, создает const.js и utils.js

---
+++ src/const.js
@@ -0,0 +1,38 @@
+export const PHOTO_URL = `http://picsum.photos/248/152?r`;
+export const MAX_COST = 1000;
+export const MAX_DAYS_GAP = 7;
+export const MAX_HOURS_GAP = 24;
+export const MAX_MINUTES_GAP = 60;
+
+export const DESTINATION_POINTS = [
+  `Moscow`,
+  `Paris`,
+  `Luxemburg`,
+  `New York`,
+  `San Francisco`
+];
+
+export const PONT_TYPES = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check-in`,
+  `Sightseeing`,
+  `Restaurant`
+];
+
+export const POINT_DESCRIPRTIONS = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`
+];
+
+export const POINT_OPTIONS = [`Add luggage`, `Add meal`, `Add breakfast`, `Choose seats`, `Travel by train`, `Switch to comfort`, `Book tickets`, `Lunch in city`, `Rent a car`, `Order Uber`];
+
+
--- src/mock/event.js
+++ src/mock/event.js
@@ -1,62 +1,42 @@
+import {getRandomInteger} from "../utils.js";
+import {DESTINATION_POINTS, PONT_TYPES, POINT_DESCRIPRTIONS, POINT_OPTIONS, PHOTO_URL, MAX_COST, MAX_DAYS_GAP, MAX_HOURS_GAP, MAX_MINUTES_GAP} from "../const.js";
 import {nanoid} from 'nanoid';
 import dayjs from 'dayjs';
 
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-const types = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
-
 const generateType = () => {
-  const randomIndex = getRandomInteger(0, types.length - 1);
-  return types[randomIndex];
+  const randomIndex = getRandomInteger(0, PONT_TYPES.length - 1);
+  return PONT_TYPES[randomIndex];
 };
-
-const destinations = [`Moscow`, `Paris`, `Luxemburg`, `New York`, `San Francisco`];
 
 const generateDestination = () => {
-  const randomIndex = getRandomInteger(0, types.length - 1);
-  return destinations[randomIndex];
+  const randomIndex = getRandomInteger(0, PONT_TYPES.length - 1);
+  return DESTINATION_POINTS[randomIndex];
 };
 
-const descriptions = [
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget.`,
-  `Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
-  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
-  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
-  `Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`];
-
 const generateDesription = () => {
-  const desriptionCount = getRandomInteger(0, descriptions.length - 1);
-  const newDescription = descriptions.slice(0, desriptionCount).join(``);
+  const desriptionCount = getRandomInteger(0, POINT_DESCRIPRTIONS.length - 1);
+  const newDescription = POINT_DESCRIPRTIONS.slice(0, desriptionCount).join(``);
 
   return newDescription;
 };
 
 const generatePhotos = () => {
   const photosCount = getRandomInteger(0, 5);
-  const photoUrl = `http://picsum.photos/248/152?r`;
   const generatePhoto = () => {
-    return `${photoUrl}=${Math.random()}`;
+    return `${PHOTO_URL}=${Math.random()}`;
   };
   const photos = new Array(photosCount).fill().map(generatePhoto);
 
   return photos;
 };
 
-const options = [`Add luggage`, `Add meal`, `Add breakfast`, `Choose seats`, `Travel by train`, `Switch to comfort`, `Book tickets`, `Lunch in city`, `Rent a car`, `Order Uber`];
-const maxCost = 1000;
-
 const generateOption = () => {
-  const desriptionOptions = getRandomInteger(0, options.length - 1);
+  const desriptionOptions = getRandomInteger(0, POINT_OPTIONS.length - 1);
 
   return {
     id: nanoid(),
-    option: options[desriptionOptions],
-    cost: getRandomInteger(1, maxCost),
+    option: POINT_OPTIONS[desriptionOptions],
+    cost: getRandomInteger(1, MAX_COST),
     isChecked: Boolean(getRandomInteger(0, 1)),
   };
 };
@@ -73,12 +53,9 @@ const generateOptions = () => {
 };
 
 const generateDate = () => {
-  const maxDaysGap = 7;
-  const maxHoursGap = 24;
-  const maxMinutesGap = 60;
-  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
-  const hoursGap = getRandomInteger(-maxHoursGap, maxHoursGap);
-  const minutesGap = getRandomInteger(-maxMinutesGap, maxMinutesGap);
+  const daysGap = getRandomInteger(-MAX_DAYS_GAP, MAX_DAYS_GAP);
+  const hoursGap = getRandomInteger(-MAX_HOURS_GAP, MAX_HOURS_GAP);
+  const minutesGap = getRandomInteger(-MAX_MINUTES_GAP, MAX_MINUTES_GAP);
 
   return dayjs().add(daysGap, `day`).add(hoursGap, `hours`).add(minutesGap, `minutes`);
 };
@@ -101,7 +78,7 @@ export const generateEvent = () => {
     },
     startTime,
     endTime,
-    cost: getRandomInteger(1, maxCost),
+    cost: getRandomInteger(1, MAX_COST),
     options: generateOptions(),
     isFavorite: Boolean(getRandomInteger(0, 1)),
   };
